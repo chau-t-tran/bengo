e4508874f73bff2cba1b7067fa94f475841d8ff8
chau-t-tran <chau.t.tran.us@gmail.com>
2022-10-31 02:27:41 -0400

    Fixed import in lexer test

--- lexer/test_lexer.go
+++ lexer/test_lexer.go
@@ -17,9 +17,9 @@ type LexerTestSuite struct {
 
 func (suite *LexerTestSuite) TestByte() {
 	input := "4:spam"
-	expected := []token.Token{
-		{Name: token.BYTE_LENGTH, Literal: "4"},
-		{Name: token.BYTE_CONTENT, Literal: "spam"},
+	expected := []token.token.Token{
+		{Type: token.BYTE_LENGTH, Literal: "4"},
+		{Type: token.BYTE_CONTENT, Literal: "spam"},
 	}
 	actual := lexer.Lex(input)
 	assert.Equal(suite.T(), expected, actual)
@@ -27,10 +27,10 @@ func (suite *LexerTestSuite) TestByte() {
 
 func (suite *LexerTestSuite) TestInteger() {
 	input := "i123e"
-	expected := []token.Token{
-		{Name: token.INT_ENTRY, Literal: "i"},
-		{Name: token.INT_VALUE, Literal: "123"},
-		{Name: token.END, Literal: "e"},
+	expected := []token.token.Token{
+		{Type: token.INT_ENTRY, Literal: "i"},
+		{Type: token.INT_VALUE, Literal: "123"},
+		{Type: token.END, Literal: "e"},
 	}
 	actual := lexer.Lex(input)
 	assert.Equal(suite.T(), expected, actual)
@@ -38,14 +38,14 @@ func (suite *LexerTestSuite) TestInteger() {
 
 func (suite *LexerTestSuite) TestList() {
 	input := "l4:spami42ee"
-	expected := []Token{
-		{Name: token.LIST_ENTRY, Literal: "l"},
-		{Name: token.BYTE_LENGTH, Literal: "4"},
-		{Name: token.BYTE_CONTENT, Literal: "spam"},
-		{Name: token.INT_ENTRY, Literal: "i"},
-		{Name: token.INT_VALUE, Literal: "42"},
-		{Name: token.END, Literal: "e"},
-		{Name: token.END, Literal: "e"},
+	expected := []token.Token{
+		{Type: token.LIST_ENTRY, Literal: "l"},
+		{Type: token.BYTE_LENGTH, Literal: "4"},
+		{Type: token.BYTE_CONTENT, Literal: "spam"},
+		{Type: token.INT_ENTRY, Literal: "i"},
+		{Type: token.INT_VALUE, Literal: "42"},
+		{Type: token.END, Literal: "e"},
+		{Type: token.END, Literal: "e"},
 	}
 	actual := lexer.Lex(input)
 	assert.Equal(suite.T(), expected, actual)
@@ -53,18 +53,18 @@ func (suite *LexerTestSuite) TestList() {
 
 func (suite *LexerTestSuite) TestDict() {
 	input := "d3:bar4:spam3:fooi42ee"
-	expected := []Token{
-		{Name: token.DICT_ENTRY, Literal: "l"},
-		{Name: token.BYTE_LENGTH, Literal: "3"},
-		{Name: token.BYTE_CONTENT, Literal: "bar"},
-		{Name: token.BYTE_LENGTH, Literal: "4"},
-		{Name: token.BYTE_CONTENT, Literal: "spam"},
-		{Name: token.BYTE_LENGTH, Literal: "3"},
-		{Name: token.BYTE_CONTENT, Literal: "foo"},
-		{Name: token.INT_ENTRY, Literal: "i"},
-		{Name: token.INT_VALUE, Literal: "42"},
-		{Name: token.END, Literal: "e"},
-		{Name: token.END, Literal: "e"},
+	expected := []token.Token{
+		{Type: token.DICT_ENTRY, Literal: "l"},
+		{Type: token.BYTE_LENGTH, Literal: "3"},
+		{Type: token.BYTE_CONTENT, Literal: "bar"},
+		{Type: token.BYTE_LENGTH, Literal: "4"},
+		{Type: token.BYTE_CONTENT, Literal: "spam"},
+		{Type: token.BYTE_LENGTH, Literal: "3"},
+		{Type: token.BYTE_CONTENT, Literal: "foo"},
+		{Type: token.INT_ENTRY, Literal: "i"},
+		{Type: token.INT_VALUE, Literal: "42"},
+		{Type: token.END, Literal: "e"},
+		{Type: token.END, Literal: "e"},
 	}
 	actual := lexer.Lex(input)
 	assert.Equal(suite.T(), expected, actual)
